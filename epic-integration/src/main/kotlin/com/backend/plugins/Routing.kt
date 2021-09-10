93f2d5ebe68f5d8dabd89bb878e8521a7fd08a20
Ole-Magnus Vian Norum <ole99@live.no>
2021-09-10 16:09:12 +0200

    Add simple template and example code

---
+++ epic-integration/src/main/kotlin/com/backend/plugins/Routing.kt
@@ -0,0 +1,32 @@
+package com.backend.plugins
+
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import kotlinx.serialization.Serializable
+
+
+@Serializable
+data class Person(val firstName: String?, val lastName: String?, val age: Int?)
+
+fun Application.personRoute() {
+
+    routing {
+        get("/person") {
+            val person = call.receive<Person>()
+            call.respondText("this is a person from json: \n ${person.firstName} ${person.lastName} is ${person.age}")
+
+            //requesting dummy epic serever endpint
+            requestEpic()
+        }
+
+        //dummy epic server endpoint
+        get("/epic") {
+            val epicResponse = "THIS IS EPICS RESPONSE"
+            call.respondText(epicResponse)
+        }
+    }
+
+}
