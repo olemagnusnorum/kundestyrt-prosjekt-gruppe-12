f46f2ca0d2daa9d934f2992131a631668c3e54d2
Ole-Magnus Vian Norum <ole99@live.no>
2021-09-17 16:22:58 +0200

    feat: add method for authentication token

--- epic-integration/src/main/kotlin/com/backend/plugins/authorization.kt
+++ epic-integration/src/main/kotlin/com/backend/plugins/authorization.kt
@@ -26,32 +26,7 @@ fun generateJWT(): String {
     //RSA private key is reformated to fit the java library used to make private key class
     // openssl pkcs8 -topk8 -in private_key.pem -inform pem -out private_key_pkcs8.pem -outform pem -nocrypt
     val myPrivateKey = """
-    MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDKx8jcxVpI+OcU
-    bc/SuBGA0mVEPHUf/OF1/xkJyR+x/kZOc85hMOk2gsMomYYL/Ie78ZhjhiQLXHPn
-    P8fEQnwQyB/vOF2OZ3bbfGHWfvX79Xf/19M6qD1phUGqvkjtjjcwEzblItgKuvTv
-    rNN1G/8aGoOtkMD5CePwCRo3bCZt09qTytsEYPzLcXZeY0CcwgUlAYdQrkxTvJjq
-    QOoG7RholX3+/YfxUhPkmML0HJddenO2hUSiIJ2yDLsbbFPZjShRNhNNkJ7uBHs7
-    eai9PeY2PfBfxU24/dulKOou4wSIlb6x0bUAdtu/xvLRNNEzWQsKYFishmkrZ7Pn
-    uicdWjjlAgMBAAECggEAUJVsYS+cXXtseVXaTOcRLZxu2dscwIWCI54omRbwHY/q
-    6KpkzYLeauc0HyJDfufwGOQ9pu9by5gGB1P1Uy0ImLKu3iYMJ9c+CKM5TkBOCL7X
-    3TgrnRfWr+Rg4DWWTbFChTxdiFl8eWcqh0SrE9L4Yas/wO3RgiZICs/TN0snSmU9
-    MB9dYsQjce/tN07IlumJ6b55xFWPubemYr1KMX/E8y1ndIpvdCu0pJsXQih9KqVd
-    7GTP84Howr3pHsWmQqhwCR8sm1DvHHXCp84yh0F6Q5jDT2QAKLUDtRZltAy96WM+
-    GlZB+lHK10NhCy7+DzaBiW+RjOwNkoyS8m3zb1QFUQKBgQDtGwuJp0b/rR+mrtNv
-    vhAyHjWNHoOOPmR8bi3ioGEuyS6zWlC9sGfXPfRg8A931LHXetQchriqP0zdTKx7
-    xEgRwIdpA46rSi7ikMhwDqfJ4K7AwVbZmWS+6IE2ljkSCqPkhLTj0MJN9sdNY31R
-    zLl/tWByPmMzY5dhUXxi8RB19wKBgQDa8IE4Qf8SzTZqNp6PqfK6+I9QeLXPWeG5
-    OOwYR6mc7GcKegkb5EAjkB2QAXVw0DMeC03L5yxhpoFQDyocVhQwdeAIHeq67frM
-    yYJe4oaGmbUFT6USIsV1fxoC1miU0Ly9ELirv/cvw+eerhSDEzlZZg2Dnnumtz1g
-    UZVFH6whAwKBgQCqSIEZxDwWRWR01hvdpZeGFvIIvoBmuOJ8BrkHiH0jF7NldZiF
-    EtsQ11rZ/65sNiHfCkSplAutoTZ3vKgnXwkPTsExJy/gTq+rYaXB7JSORMHchz3z
-    mtmizeJ2rJ6iGWgpJzGuZ3AUhAYQy0HmbGxQjpeMlMez0XOXnekCOfTDcwKBgFWQ
-    ux/Ogy7I7uRR1mRVSh0SeYg8tOmFujcWbQcMMti2d4rbqOWyrfJ4J2WZfFmLtP6f
-    8lanUWwUk+NDGfUbGybZWqYxcwR9b5BLjN0icOr39YxO1TeiSUOjWfoT66mFbs4B
-    U/sjuip7yPD2HeQgRkCPJubysGa2pJEZMVpjh3UbAoGBALTy9NUK9S8dpu0bNHjV
-    YuoYKbFvtt1sTPSwLqNCa9aNpa/mAdfAUam9EyN/qBoDkH7hG/ivtSJbCn9dIbHJ
-    lEaaRsis1Cc1MiNM/KRU2xLz+sp+G2CNJtpMwYQhWv2dYQU19YiHPfEajPRCRcCU
-    T84bdDBT4rj0ijvmX0FxHJPq
+    dummy-private-key
     """
 
     // removing white space
