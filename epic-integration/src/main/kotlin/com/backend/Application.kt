f67d62a5b37fb17d7abbe105e99d177640f95ade
Ole-Magnus Vian Norum <ole99@live.no>
2021-09-23 12:53:48 +0200

    feat: function to get person data from epic

--- epic-integration/src/main/kotlin/com/backend/Application.kt
+++ epic-integration/src/main/kotlin/com/backend/Application.kt
@@ -4,19 +4,16 @@ import com.backend.plugins.*
 
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import com.backend.plugins.*
-import com.sun.net.httpserver.Authenticator
-import io.jsonwebtoken.Jwts
-import io.jsonwebtoken.SignatureAlgorithm
-import io.jsonwebtoken.security.Keys
+
 import freemarker.cache.ClassTemplateLoader
 import io.ktor.application.*
 import io.ktor.features.*
 import io.ktor.freemarker.*
 import io.ktor.serialization.*
-import kotlinx.coroutines.launch
+
+import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.json.Json
-import java.security.Key
+
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0", watchPaths = listOf("classes", "resources")) {
@@ -31,6 +28,8 @@ fun main() {
         install(FreeMarker) {
             templateLoader = ClassTemplateLoader(this::class.java.classLoader, "templates")
         }
+
+        runBlocking { requestEpicPatient("Derek", "Lin", "1973-06-03") }
 
         personRoute()
     }.start(wait = true)
