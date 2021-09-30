e5107cdfe000b87f81505233d2f8c2c7b4234f1e
Magnus <magvaag@yahoo.no>
2021-09-30 13:42:49 +0200

    fix: fix test import

--- epic-integration/src/test/kotlin/com/backend/EpicCommunicationTest.kt
+++ epic-integration/src/test/kotlin/com/backend/EpicCommunicationTest.kt
@@ -1,10 +1,11 @@
 package com.backend
 
 import com.backend.plugins.EpicCommunication
+import kotlinx.coroutines.runBlocking
 import org.hl7.fhir.r4.model.Patient
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.TestInstance
+import kotlin.test.*
 
 
 // Warning: PER_CLASS Lifecycle means that the same EpicCommunicationTest class is used for every nested test
@@ -15,16 +16,15 @@ class EpicCommunicationTest {
 
     @Nested
     inner class PatientSearch {
-        private lateinit var patientXML: String
 
         @Test
-        suspend fun `Patient_Search should return a string`() {
-            patientXML = epicCommunication.patientSearch(given = "Derrick", family = "Lin", birthdate = "1973-06-03")
-            assert(patientXML is String)
+        fun `Patient_Search should return a string`() {
+            assert(runBlocking { epicCommunication.patientSearch(given = "Derrick", family = "Lin", birthdate = "1973-06-03") } is String)
         }
 
         @Test
         fun `parseBundleXMLToPatient should parse an xml string to a patient object`() {
+            val patientXML = runBlocking { epicCommunication.patientSearch(given = "Derrick", family = "Lin", birthdate = "1973-06-03") }
             assert(epicCommunication.parseBundleXMLToPatient(patientXML) is Patient)
         }
     }
