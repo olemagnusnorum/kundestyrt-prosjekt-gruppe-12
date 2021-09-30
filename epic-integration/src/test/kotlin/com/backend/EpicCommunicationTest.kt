b85f4674d6da7376f6fd4cc72d60ed1b8fbf5cbb
Magnus's Mac <magnusvaagemorud@gmail.com>
2021-09-30 12:05:32 +0200

    test: test xml-to-patient parser and refactor EpicCommunicationTest class

--- epic-integration/src/test/kotlin/com/backend/EpicCommunicationTest.kt
+++ epic-integration/src/test/kotlin/com/backend/EpicCommunicationTest.kt
@@ -1,15 +1,32 @@
 package com.backend
 
 import com.backend.plugins.EpicCommunication
+import org.hl7.fhir.r4.model.Patient
 import org.junit.Test
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.TestInstance
 
+
+// Warning: PER_CLASS Lifecycle means that the same EpicCommunicationTest class is used for every nested test
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class EpicCommunicationTest {
 
     private val epicCommunication = EpicCommunication()
 
-    @Test
-    suspend fun `Patient_Search returns a string`() {
-        assert(epicCommunication.patientSearch(given = "Derrick", family = "Lin", birthdate = "1973-06-03") is String)
+    @Nested
+    inner class PatientSearch {
+        private lateinit var patientXML: String
+
+        @Test
+        suspend fun `Patient_Search should return a string`() {
+            patientXML = epicCommunication.patientSearch(given = "Derrick", family = "Lin", birthdate = "1973-06-03")
+            assert(patientXML is String)
+        }
+
+        @Test
+        fun `parseBundleXMLToPatient should parse an xml string to a patient object`() {
+            assert(epicCommunication.parseBundleXMLToPatient(patientXML) is Patient)
+        }
     }
 
 }
