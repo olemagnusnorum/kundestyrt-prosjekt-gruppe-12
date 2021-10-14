2fe1d41993cfd85f6e4d251913a7b8e6770a040c
Magnus <magvaag@yahoo.no>
2021-10-14 12:29:04 +0200

    feat: add patient-login

--- epic-integration/src/main/kotlin/com/backend/plugins/RoutingVenterBarn.kt
+++ epic-integration/src/main/kotlin/com/backend/plugins/RoutingVenterBarn.kt
@@ -1,10 +1,12 @@
 package com.backend.plugins
 
 import io.ktor.application.*
+import io.ktor.client.call.*
 import io.ktor.freemarker.*
 import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
+import kotlinx.coroutines.runBlocking
 
 
 fun Application.venterBarnRoute() {
@@ -12,6 +14,17 @@ fun Application.venterBarnRoute() {
         route("/venter-barn") {
             get {
                 call.respondTemplate("/venter-barn/index.ftl")
+            }
+
+            get("/patient-login") {
+                call.respondTemplate("/venter-barn/patient-login.ftl")
+            }
+
+            get("/patient") {
+                val params = call.receiveParameters()
+                val id = params["id"]
+                val patient = runBlocking { epicCommunication.parsePatientStringToObject(epicCommunication.readPatient(id!!).receive()) }
+                call.respondTemplate("/venter-barn/patient-login.ftl")
             }
         }
 
---
+++ epic-integration/src/main/resources/templates/venter-barn/patient.ftl
@@ -0,0 +1,27 @@
+<#ftl output_format="HTML">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Legeside</title>
+</head>
+<body style="text-align: center; font-family: sans-serif">
+<div>
+    <h1>Pasient Journal</h1>
+    <h2>Hei ${name}!</h2>
+    <h3>Se melding fra NAV</h3>
+    <#if condition??>
+        <p>${condition}</p>
+        <p><b>Termindato:</b> ${due_date}</p>
+
+        <form action="/" method="get">
+            <input type="submit" value="Søk foreldrepenger">
+        </form>
+    <#else>
+        <p>Du kan ikke søke om foreldrepenger.</p>
+    </#if>
+    <br>
+    <br>
+    <a href="/">Gå tilbake til navigasjonsiden.</a>
+</div>
+</body>
+</html>
